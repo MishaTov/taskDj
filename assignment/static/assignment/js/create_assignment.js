1c7a4a1eab5b99aa2e02bbe6941b1a503335f8af
MishaTov <yamishanya1587@gmail.com>
2024-09-20 16:58:53 +0300

    updated

--- assignment/static/assignment/js/create_assignment.js
+++ assignment/static/assignment/js/create_assignment.js
@@ -5,6 +5,14 @@ for (let element of assignmentForm) {
     element.addEventListener('invalid', (event) => {
         event.preventDefault();
         element.classList.add('invalid');
-        console.log(element.validationMessage);
+        const errorMsg = element.parentNode.querySelector('.field-error-msg');
+        if (!errorMsg){
+            const errorMsg = document.createElement('p');
+            errorMsg.classList.add('field-error-msg');
+            errorMsg.innerHTML = element.validationMessage;
+            element.parentNode.appendChild(errorMsg);
+        } else {
+            errorMsg.innerHTML = element.validationMessage;
+        }
     })
 }
--- assignment/views.py
+++ assignment/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 
 from .forms import AssignmentForm, FileForm
-from .models import File
+from .models import File, Assignment
 
 
 def main_page(request):
@@ -44,3 +44,6 @@ class CreateAssignment(View):
                        'file_form': file_form}
             return render(request, 'assignment/create_assignment.html', context=context)
         return redirect('assignment_list')
+
+
+
