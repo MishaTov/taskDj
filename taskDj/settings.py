4c6b30ecb73a1ca9ae8ad786801e7b1d19cb74fc
MishaTov <yamishanya1587@gmail.com>
2024-11-06 19:05:11 +0200

    docker and nginx

--- taskDj/settings.py
+++ taskDj/settings.py
@@ -125,8 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static/',
+    BASE_DIR / 'c_static/',
 ]
 
 # Default primary key field type
